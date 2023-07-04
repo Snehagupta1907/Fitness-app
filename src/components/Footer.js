14ca02c7fb72990989d732f0f2d6f3ab4f4224c2
Snehagupta1907 <snehagupta98930@gmail.com>
2023-07-04 22:04:06 +0530

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,31 @@
+import { GitHub, Instagram, LinkedIn } from '@mui/icons-material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box sx={{ bgcolor: '#653b46', p: 3, mt: 'auto' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+      <Link href="https://www.linkedin.com/in/sneha-gupta-239aa1201/" target="_blank" rel="noopener">
+        <LinkedIn sx={{ color: 'pink',mr: 1 }} color='pink' />
+      </Link>
+      <Link href="https://github.com/Snehagupta1907" target="_blank" rel="noopener">
+        <GitHub sx={{ color: 'pink',mx: 1 }} />
+      </Link>
+      <Link href="https://www.instagram.com/" target="_blank" rel="noopener">
+        <Instagram sx={{ color: 'pink',ml: 1 }} />
+      </Link>
+    </Box>
+    <Typography variant="body2" color="white" align="center">
+      &copy; {new Date().getFullYear()} App Name. All rights reserved.
+    </Typography>
+    <Typography variant="body2" color="white" align="center">
+      Made by Sneha Gupta
+    </Typography>
+  </Box>
+  );
+};
 
 export default Footer
